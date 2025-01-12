f41186aa65b0a40696b3f1c90c1fc436332ef7ab
unknown <fathur.risqo@gmail.com>
2025-01-12 12:14:52 +0700

    Learn fetching data

--- app/lib/definitions.ts
+++ app/lib/definitions.ts
@@ -23,6 +23,7 @@ export type Invoice = {
   date: string;
   // In TypeScript, this is called a string union type.
   // It means that the "status" property can only be one of the two strings: 'pending' or 'paid'.
+  // 
   status: 'pending' | 'paid';
 };
 
