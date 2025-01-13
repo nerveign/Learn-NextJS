3dcb833e769d68dec3d2c8add2955fbb58750892
unknown <fathur.risqo@gmail.com>
2025-01-13 23:34:01 +0700

    Add CRUD Feature on invoices page

--- app/lib/definitions.ts
+++ app/lib/definitions.ts
@@ -16,8 +16,8 @@ export type Invoice = {
   id: string;
   customer_id: string;
   amount: number;
-  date: string;
   status: 'pending' | 'paid';
+  date: string;
 };
 
 export type Revenue = {
--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -2,7 +2,7 @@ import { CustomerField } from '@/app/lib/definitions';
 import Link from 'next/link';
 import { CheckIcon, ClockIcon, CurrencyDollarIcon, UserCircleIcon } from '@heroicons/react/24/outline';
 import { Button } from '@/app/ui/button';
-import createInvoice from '@/app/lib/action';
+import { createInvoice } from '@/app/lib/action';
 
 export default function Form({ customers }: { customers: CustomerField[] }) {
   return (
